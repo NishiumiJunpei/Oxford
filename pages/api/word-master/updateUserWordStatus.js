a2e22eb84188c9214b7c323c0315e309e65113d3
西海隼平 <nishiumi@gmail.com>
2023-11-23 11:17:45 +0900

    update

--- pages/api/word-master/updateUserWordStatus.js
+++ pages/api/word-master/updateUserWordStatus.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
       const userId = session.userId; // セッションから userId を取得
       const { wordId, status } = req.body;
 
+      console.log('test',userId, wordId, status)
       const response = await updateUserWordStatus(userId, wordId, status);
       console.log('res', response)
       res.status(200).json(response);
