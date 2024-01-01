eb27639471bbd2cac5c285fd4ef60baf4cb050ed
西海隼平 <nishiumi@gmail.com>
2024-01-01 11:19:37 +0900

    update

--- pages/word-master/learnWordsCheck.js
+++ pages/word-master/learnWordsCheck.js
@@ -1,21 +1,116 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
-import { Typography, Button, Box, CircularProgress } from '@mui/material';
+import { Typography, Button, Box, CircularProgress, Container,Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import Link from 'next/link';
 import CloseIcon from '@mui/icons-material/Close'; // 終了アイコンのインポート
 
+
+const FinishLearnWordsCheck = ({blockId, notMemorizedWordList}) =>{
+  const router = useRouter();
+  const [wordDetail, setWordDetail] = useState(null);
+
+
+  // 画像ファイル名の配列
+  const images = [
+    'learnWordsCheckCompletionImage1.png',
+    'learnWordsCheckCompletionImage2.png',
+    'learnWordsCheckCompletionImage3.png',
+    'learnWordsCheckCompletionImage4.png',
+    'learnWordsCheckCompletionImage5.png',
+    'learnWordsCheckCompletionImage6.png',
+    'learnWordsCheckCompletionImage7.png',
+    'learnWordsCheckCompletionImage8.png',
+    'learnWordsCheckCompletionImage9.png',
+    'learnWordsCheckCompletionImage10.png',
+  ];
+
+  // ランダムに画像を選択
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
+  return (
+    <Container maxWidth="sm">
+    <Box 
+      display="flex" 
+      flexDirection="column" 
+      alignItems="center" 
+      justifyContent="flex-start" // このプロパティを 'flex-start' に変更
+      height="100vh"
+      marginTop={2} // 上にマージンを追加
+    >
+      <Typography variant="h4" gutterBottom>
+        よくできました！
+      </Typography>
+      {/* <img width="300" src={`/images/${randomImage}`} alt="Completion" /> */}
+      {notMemorizedWordList?.length > 0 && (
+        <>
+          <Typography variant="h4" gutterBottom>
+            わからなかった単語
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>英語</TableCell>
+                  <TableCell>日本語</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {notMemorizedWordList?.map((word) =>
+                    <TableRow key={word.id} hover onClick={() => setWordDetail(word)}>
+                      <TableCell>{word.english}</TableCell>
+                      <TableCell>{word.japanese}</TableCell>
+                    </TableRow>
+                  )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+
+        {wordDetail && (
+          <Box sx={{mb: 5}}>
+            <h2>{wordDetail.english}</h2>
+            <p>{wordDetail.japanese}</p>
+            <p> {wordDetail.exampleSentence}</p>
+            {wordDetail.image && <img src={wordDetail.image} alt={wordDetail.english} />}
+
+            {wordDetail?.imageUrl && (
+                <img 
+                    src={wordDetail.imageUrl} 
+                    style={{ marginTop: 20, maxWidth: '100%', maxHeight: '50%', objectFit: 'contain' }} 
+                />
+            )}
+
+          </Box>
+        )}
+
+
+      <Link href={`/word-master/wordList?blockId=${blockId}`} passHref>
+        <Button variant="default" color="primary" sx={{marginTop: 3}}>
+          戻る
+        </Button>
+      </Link>
+    </Box>
+  </Container>
+  )
+}
 
 const LearnWordsCheck = () => {
     const router = useRouter();
     const { blockId, wordCount, languageDirection, includeMemorized } = router.query;
     const [isLoading, setIsLoading] = useState(false);
     const [wordList, setWordList] = useState([]);
+    const [notMemorizedWordList, setNotMemorizedWordList] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
     const [showAnswer, setShowAnswer] = useState(false);
     const [showAnswerButton, setShowAnswerButton] = useState(true);
     const [result, setResult] = useState({ known: 0, unknown: 0 });
     const [buttonDisabled, setButtonDisabled] = useState(false);
     const [nextButtonDisabled, setNextButtonDisabled] = useState(true); // 次へボタンの無効化状態
+    const [isFinish, setIsFinish] = useState(false);
+
+    console.log('test', notMemorizedWordList)
   
     useEffect(() => {
       // URLクエリパラメータを使用してAPIから単語データを取得する
@@ -44,10 +139,11 @@ const LearnWordsCheck = () => {
           setNextButtonDisabled(true);
         }
       } else {
-        router.push({
-          pathname: '/word-master/learnWordsCheckCompletion',
-          query: { blockId}
-        });
+        setIsFinish(true)
+        // router.push({
+        //   pathname: '/word-master/learnWordsCheckCompletion',
+        //   query: { blockId}
+        // });
       }
     };
     
@@ -64,6 +160,7 @@ const LearnWordsCheck = () => {
             setShowAnswer(true);
             setButtonDisabled(true);
             setNextButtonDisabled(false);
+            setNotMemorizedWordList([...notMemorizedWordList, {...word}])
           }    
         }
         else{
@@ -152,39 +249,46 @@ const LearnWordsCheck = () => {
   }
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 10, height: 'calc(100vh - 10px)' }}>
-        <Button onClick={handleExit} sx={{ position: 'absolute', top: 20, right: 20, zIndex: 1500 }}>
-          <CloseIcon />
-        </Button>
-
-        
-        <Typography variant="h6" sx={{ marginBottom: 2 }}>残りの問題数: {remainingWords}</Typography>
-        <Typography variant="h5" sx={{ marginBottom: 2, fontWeight: 'bold', fontSize: '2rem', color: 'primary.main' }}>
-          {languageDirection == 'EJ' ? word.english : word.japanese}
-        </Typography>
-        {showAnswer &&
-           <Typography variant="subtitle1" sx={{ marginBottom: 2 }}>
-             {languageDirection == 'EJ' ? word.japanese : word.english}
-          </Typography>}
-        <Box sx={{ '& > button': { margin: 1 } }}>
-            <Button variant="contained" color="primary" onClick={() => handleAnswer(true)} disabled={buttonDisabled}>
-                わかる(j)
-            </Button>
-            <Button variant="contained" color="secondary" onClick={() => handleAnswer(false)} disabled={buttonDisabled}>
-                わからない(l)
-            </Button>
-        </Box>
-        <Box>
-          <Button variant="outlined" onClick={handleShowAnswer} disabled={!showAnswerButton}>
-              答えチェック(k)
+    <>
+      {isFinish ? (
+        <FinishLearnWordsCheck blockId={blockId} notMemorizedWordList={notMemorizedWordList}/>
+      ):(
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 10, height: 'calc(100vh - 10px)' }}>
+          <Button onClick={handleExit} sx={{ position: 'absolute', top: 20, right: 20, zIndex: 1500 }}>
+            <CloseIcon />
           </Button>
-        </Box>
-        {showAnswer && (
-            <Button variant="contained" onClick={handleNext} disabled={nextButtonDisabled} sx={{ marginTop: 2 }}>
-                次へ(n)
+
+          
+          <Typography variant="h6" sx={{ marginBottom: 2 }}>残りの問題数: {remainingWords}</Typography>
+          <Typography variant="h5" sx={{ marginBottom: 2, fontWeight: 'bold', fontSize: '2rem', color: 'primary.main' }}>
+            {languageDirection == 'EJ' ? word.english : word.japanese}
+          </Typography>
+          {showAnswer &&
+            <Typography variant="subtitle1" sx={{ marginBottom: 2 }}>
+              {languageDirection == 'EJ' ? word.japanese : word.english}
+            </Typography>}
+          <Box sx={{ '& > button': { margin: 1 } }}>
+              <Button variant="contained" color="primary" onClick={() => handleAnswer(true)} disabled={buttonDisabled}>
+                  わかる(j)
+              </Button>
+              <Button variant="contained" color="secondary" onClick={() => handleAnswer(false)} disabled={buttonDisabled}>
+                  わからない(l)
+              </Button>
+          </Box>
+          <Box> 
+            <Button variant="outlined" onClick={handleShowAnswer} disabled={!showAnswerButton}>
+                答えチェック(k)
             </Button>
-        )}
-    </Box>
+          </Box>
+          {showAnswer && (
+              <Button variant="contained" onClick={handleNext} disabled={nextButtonDisabled} sx={{ marginTop: 2 }}>
+                  次へ(n)
+              </Button>
+          )}
+      </Box>
+
+      )}
+    </>
 );
 
 };
