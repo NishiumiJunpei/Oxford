8a349dd1430c42b75296fac033b8c3402ae16ee1
西海隼平 <nishiumi@gmail.com>
2024-01-29 21:15:47 +0900

    update

---
+++ pages/api/script/createSentenceE.js
@@ -0,0 +1,31 @@
+import { createExampleSentenceAndImageByGPT } from '@/utils/wordList-utils';
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
+export default async function handler(req, res) {
+  try {
+    const wordLists = await prisma.wordList.findMany({
+      where: {
+        exampleSentenceE: '',
+      },
+    });
+
+
+    for (let i = 0; i < wordLists.length; i++) {
+      const wordList = wordLists[i];
+
+      if (!wordList.exampleSentenceE){
+        await createExampleSentenceAndImageByGPT(wordList.id)
+        console.log(`Completed: ${i} / ${wordLists.length} --- ${wordList.id}.${wordList.english}`)  
+      }
+
+    }
+
+
+    res.status(200).json('done');
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error', error: error.message });
+  }
+}
