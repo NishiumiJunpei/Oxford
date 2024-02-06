55b9c54dbfe75c11b7390604ef2d1d383f3a0985
西海隼平 <nishiumi@gmail.com>
2024-02-06 19:51:07 +0900

    update

--- utils/variables.js
+++ utils/variables.js
@@ -5,13 +5,13 @@ export const challengeThemes = [
     { id: 6, name: '英検準２級', imageFilename: 'eiken2-sub.png', activeStatus: 'ACTIVE' },
     { id: 7, name: '英検３級', imageFilename: 'eiken3.png', activeStatus: 'ACTIVE' },
     { id: 4, name: '英検４級', imageFilename: 'eiken4.png', activeStatus: 'ACTIVE' },
+    { id: 11, name: 'TOEIC 900レベル', imageFilename: 'TOEIC900.png', activeStatus: 'PREPARING' },
+    { id: 12, name: 'TOEIC 800レベル', imageFilename: 'TOEIC800.png', activeStatus: 'PREPARING' },
+    { id: 13, name: 'TOEIC 700レベル', imageFilename: 'TOEIC700.png', activeStatus: 'PREPARING' },
+    { id: 14, name: 'TOEIC 600レベル', imageFilename: 'TOEIC600.png', activeStatus: 'PREPARING' },
     { id: 96, name: 'ビジネス英語 基礎', imageFilename: 'business-english1.png', activeStatus: 'PREPARING' },
     { id: 95, name: 'ビジネス英語 中級', imageFilename: 'business-english2.png', activeStatus: 'PREPARING' },
     { id: 94, name: 'ビジネス英語 上級', imageFilename: 'business-english3.png', activeStatus: 'PREPARING' },
-    { id: 93, name: 'TOEIC 900レベル', imageFilename: 'TOEIC900.png', activeStatus: 'PREPARING' },
-    { id: 93, name: 'TOEIC 800レベル', imageFilename: 'TOEIC800.png', activeStatus: 'PREPARING' },
-    { id: 93, name: 'TOEIC 700レベル', imageFilename: 'TOEIC700.png', activeStatus: 'PREPARING' },
-    { id: 93, name: 'TOEIC 600レベル', imageFilename: 'TOEIC600.png', activeStatus: 'PREPARING' },
   ];
 
 
