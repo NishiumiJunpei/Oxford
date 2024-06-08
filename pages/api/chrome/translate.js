ae64b8bc696ffa4fa3e7d2bcfd721d68ce459d9a
西海隼平 <nishiumi@gmail.com>
2024-06-08 19:34:43 +0900

    update

--- pages/api/chrome/translate.js
+++ pages/api/chrome/translate.js
@@ -2,11 +2,14 @@
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-    //   const { userId } = await getUserFromSession(req, res);
+    //   const { userId } = await getUserFromSession(req, res)
 
-      res.status(200).json({        
-        'susuEnglish is great!!',
-      });
+    const result = {
+        originalMsg: 'test',
+        jap: 'susuEnglish is great',
+    }
+
+      res.status(200).json(result);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
