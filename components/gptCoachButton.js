2bde9eab3c7caf92c6c9763ff35abff9d7d16441
西海隼平 <nishiumi@gmail.com>
2024-07-06 08:58:34 +0900

    update

--- components/gptCoachButton.js
+++ components/gptCoachButton.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Button } from '@mui/material';
 
+const themesForGPT = [
+  'スポーツ',
+  '料理',
+  '音楽',
+  '映画',
+  '旅行',
+  'テクノロジー',
+  '健康とフィットネス',
+  '読書',
+  '趣味',
+  '教育'
+];
+
 const GPTCoachButton = ({ words }) => {
+  const getRandomTheme = () => {
+    const randomIndex = Math.floor(Math.random() * themesForGPT.length);
+    return themesForGPT[randomIndex];
+  };
 
   const handleCopyAndOpenURL = async () => {
+    const randomTheme = getRandomTheme();
+    const themeText = `テーマ：${randomTheme}\n`;
     const wordsText = words.map(word => `${word.english}, ${word.japanese}`).join('\n');
+    const fullText = themeText + wordsText;
 
     try {
-      await navigator.clipboard.writeText(wordsText);
+      await navigator.clipboard.writeText(fullText);
       window.open('https://chatgpt.com/g/g-fcRjeqACa-super-cute-coach', '_blank');
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
   };
 
