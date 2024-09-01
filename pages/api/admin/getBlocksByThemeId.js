07da1abc6155f89a0adcedaa156df07e8b2b2c12
西海隼平 <nishiumi@gmail.com>
2024-09-02 08:08:16 +0900

    update

--- pages/api/admin/getBlocksByThemeId.js
+++ pages/api/admin/getBlocksByThemeId.js
@@ -55,6 +55,8 @@ export default async function handler(req, res) {
         }));
       }
 
+      console.log('api test', updatedBlocks)
+
       res.status(200).json({ blocks: updatedBlocks });
 
     } catch (error) {
